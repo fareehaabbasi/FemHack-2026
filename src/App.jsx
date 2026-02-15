468d621e5d11b3a0b255b2297e0323003594e765
fareehaabbasi <abbasifareeha9@gmail.com>
2026-02-15 11:42:25 +0500

    dashbord-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Auth from "./Components/Auth";
+import Dashboard from "./Pages/Dashboard";
 
 const App = () => {
   return (
@@ -10,8 +11,8 @@ const App = () => {
       <Navbar />
 
       <Routes >
-        {/* <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/lostfound" element={<LostFound />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      {/* <Route path="/lostfound" element={<LostFound />} />
       <Route path="/complaints" element={<Complaints />} />
       <Route path="/volunteer" element={<Volunteer />} /> */}
       <Route path="/Authentication" element={<Auth />} />
